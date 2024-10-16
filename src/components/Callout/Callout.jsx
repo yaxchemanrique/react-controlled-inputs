be43ce2a4c533f28fd1d6a4ff482fe58449a65ac
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-16 11:59:44 -0700

    fis: unused variables

--- src/components/Callout/Callout.jsx
+++ src/components/Callout/Callout.jsx
@@ -2,27 +2,16 @@ import { MessageSquare, AlertTriangle, AlertCircle } from "feather-icons-react";
 import styles from "./Callout.module.css";
 
 function Callout({ type, children }) {
-  let bgColor = "";
-  let accentColor = "";
   let Icon = null;
   switch (type) {
     case "info":
-      bgColor = "var(--clr-accent-100-100)";
-      accentColor = "var(--clr-accent-400-100)";
       Icon = MessageSquare;
       break;
     case "warning":
-      bgColor = "var(--clr-accent-100-100)";
-      accentColor = "var(--clr-accent-400-100)";
       Icon = AlertTriangle;
       break;
     case "error":
-      bgColor = "var(--clr-accent-100-100)";
-      accentColor = "var(--clr-accent-400-100)";
       Icon = AlertCircle;
-      break;
-
-    default:
       break;
   }
 
