9728bb24e682d34ef14016fdfb4f436158c89782
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-16 15:35:34 -0700

    feat: implementation of iframes for codesandbox embeds

--- src/components/IFrameContainer/IFrameContainer.jsx
+++ src/components/IFrameContainer/IFrameContainer.jsx
@@ -1,10 +1,11 @@
 import styles from "./IFrameContainer.module.css";
 
-function IFrameContainer({ srcCode, title }) {
+function IFrameContainer({ height='auto', srcCode, title }) {
   return (
     <div className={styles.iframeContainer}>
       <iframe
       className={styles.iframe}
+      style={{height: height}}
         src={`https://codesandbox.io/embed/${srcCode}?view=split&hidenavigation=1&editorsize=58`}
         title={title}
         allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
