77872a2bf5da19d42f33177d1a0ba9cfdc1a24bc
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-18 16:17:49 -0700

    feat: reset button on code playgrounds

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -1,33 +1,54 @@
-import Editor from "@monaco-editor/react";
-import { LiveProvider, LiveError, LivePreview } from "react-live";
-import styles from "./CodeEditor.module.css";
 import { useState } from "react";
+import { LiveProvider, LiveError, LivePreview } from "react-live";
+import Editor from "@monaco-editor/react";
+import { RefreshCw } from "feather-icons-react";
 
-function CodeEditor({ defaultCode, height='auto' }) {
+import styles from "./CodeEditor.module.css";
+
+function CodeEditor({ defaultCode, height = "auto" }) {
   const [code, setCode] = useState(defaultCode);
+  
   return (
     <LiveProvider enableTypeScript={false} code={code}>
-      <div className={styles.codeEditor} style={{height: height, aspectRatio: `${height !== 'auto' ? 'auto': '16 / 9'}`}}>
-        <Editor
-          width="100%"
-          defaultLanguage="javascript"
-          defaultValue={code.trim()}
-          theme="vs-dark"
-          options={{
-            padding: {
-              top: 16,
-            },
-            lineNumbers: "off",
-            tabSize: 2,
-            fontSize: 14,
-            minimap: {
-              enabled: false,
-            },
-            contextmenu: false,
+      <div className={styles.playgroundWrapper}>
+        <div className={styles.navigationBar}>
+          <p>playground</p>
+          <button
+            onClick={() => {
+              setCode(defaultCode);
+            }}
+          >
+            <RefreshCw size={18} color="var(--clr-accent-400-100)" />
+          </button>
+        </div>
+        <div
+          className={styles.codeEditor}
+          style={{
+            height: height,
+            aspectRatio: `${height !== "auto" ? "auto" : "16 / 9"}`,
           }}
-          onChange={(value) => setCode(value)}
-        />
-        <LivePreview className={styles.preview} />
+        >
+          <Editor
+            width="100%"
+            defaultLanguage="javascript"
+            value={code.trim()}
+            theme="vs-dark"
+            options={{
+              padding: {
+                top: 16,
+              },
+              lineNumbers: "off",
+              tabSize: 2,
+              fontSize: 14,
+              minimap: {
+                enabled: false,
+              },
+              contextmenu: false,
+            }}
+            onChange={(value) => setCode(value)}
+          />
+          <LivePreview className={styles.preview} />
+        </div>
       </div>
       <LiveError />
     </LiveProvider>
