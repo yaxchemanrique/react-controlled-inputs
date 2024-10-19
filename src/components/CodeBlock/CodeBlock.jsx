4dec8b4e8a415e6641b20a5f7c376f48f0112291
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-18 23:01:08 -0700

    feat: added highlighted lines to CodeBlock

--- src/components/CodeBlock/CodeBlock.jsx
+++ src/components/CodeBlock/CodeBlock.jsx
@@ -1,11 +1,28 @@
 import { highlight } from "sugar-high";
-import styles from './CodeBlock.module.css'
+import styles from "./CodeBlock.module.css";
 
-function CodeBlock({ children }) {
+function CodeBlock({ children, highlightedLines = [] }) {
   const code = highlight(children);
+  const highlightedCode = code.split("\n").map((line, index) => {
+    const lineNumber = index + 1;
+
+    if (highlightedLines.includes(lineNumber)) {
+      return line.replace(
+        '<span class="sh__line"',
+        `<span class="${styles.sh__line} ${styles.highlight}"`
+      );
+    } else {
+      return line.replace(
+        '<span class="sh__line"',
+        `<span class=${styles.sh__line}`
+      );
+    }
+
+  }).join("\n");
+
   return (
     <pre className={styles.codeBlock}>
-      <code dangerouslySetInnerHTML={{ __html: code }} />
+      <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
     </pre>
   );
 }
